8a7f6e0f45d6ad879653476f65dd5bd03ae09edc
francochiapello <74989176+francochiapello@users.noreply.github.com>
2021-01-14 16:49:24 -0300

    Add files via upload

---
+++ shoesFront/js/index.js
@@ -0,0 +1,12 @@
+function red() {
+    var image = document.getElementById('shoeback');
+    image.src = "../img/NIKEROJA.PNG";
+}
+function black() {
+    var image = document.getElementById('shoeback');
+    image.src = "../img/NIKENEGRA.PNG";
+}
+function white(){
+    var image = document.getElementById('shoeback');
+    image.src = "../img/NIKEBLANCA.PNG";
+}
---
+++ shoesFront/js/shoe.js
@@ -0,0 +1,16 @@
+function img1() {
+    var image = document.getElementById('img');
+    image.src = "../img/shoe/shoe1.PNG";
+}
+function img2() {
+    var image = document.getElementById('img');
+    image.src = "../img/shoe/shoe2.PNG";
+}
+function img3(){
+    var image = document.getElementById('img');
+    image.src = "../img/shoe/shoe3.PNG";
+}
+
+function goBack() {
+    window.history.back();
+  }
